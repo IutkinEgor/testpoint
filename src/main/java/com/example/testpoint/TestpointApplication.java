2d0d332b9a370ab577fa8ca08b684b955e915614
Iutkin Egor <iutkin-ed@mail.ru>
2023-04-22 22:04:21 +0900

    [ INIT ]

---
+++ src/main/java/com/example/testpoint/TestpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testpoint/TestpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
